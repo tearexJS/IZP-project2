cf30a87db9608cf0d327b9b93217154f60de314d
tearexJS <olinovak96@gmail.com>
2021-12-04 17:25:51 +0100

    func-pointers somewhat ready

--- setcal.c
+++ setcal.c
@@ -23,7 +23,7 @@
             return errCode;       \
         } while (0);              \
     }
-enum State
+enum StateRelation
 {
     waitForLParentheses,
     readFirstElement,
@@ -31,11 +31,17 @@ enum State
     readSecondElement,
     waitForRParentheses
 };
+enum StateSet
+{
+    waitForSpace,
+    readElement
+};
 enum Type
 {
     UNI,
     SET,
     REL,
+    OUT,
     COMMAND
 };
 
@@ -56,22 +62,15 @@ typedef struct
     int length;
     char **content;
 } Set;
-
 typedef struct
 {
     char *name;
 
-    Row *rows;
     int arg1;
     int arg2;
+    int arg3;
 } CommandProperties;
 
-typedef struct
-{
-    char *name;
-    int (*func)(CommandProperties);
-    int argc;
-} Command;
 
 typedef struct
 {
@@ -81,21 +80,25 @@ typedef struct
         Set set;
         Relation relation;
         CommandProperties command;
+        bool outputValue; 
     };
     char *line;
 } Row;
-const Command commandList[NUMBER_OF_COMMANDS] = 
+typedef struct
 {
-    {"empty", setIsEmpty, 1},
-    {"complement", setComplement, 1},
-    {"card", setCard, 1},
-    {"union", setUnion, 2},
-    {"intersect", setIntersect, 2},
-    {"minus", setMinus, 2},
-    {"subseteq", setIsSubsetOrEq, 2},
-    {"subset", setIsSubset, 2},
-    {"equals", setEquals, 2}
-};
+    char *name;
+    int (*func)(CommandProperties, Row **);
+    int argc;
+} Command;
+
+void replaceSpaceWithZero(char *line)
+{
+    for(int i = 0; line[i] != '\0'; i++)
+    {
+        if(line[i] == ' ' || line[i] == ')')
+            line[i] = '\0';
+    }
+}
 // void printSet(Set *set)
 // {
 //     printf("%c ", set->type);
@@ -215,9 +218,12 @@ int strToInt(char *str)
 }
 void printSetContent(char **content, int length)
 {
-    for(int i = 0; i < length; i++)
+    if(length > 0)
     {
-        printf("%s ", content[i]);
+        for(int i = 0; i < length; i++)
+        {
+            printf("%s ", content[i]);
+        }
     }
     printf("\n");
 }
@@ -250,7 +256,10 @@ void print(Row *rows, int rowsCount)
             printf("R ");
             printRelContent(rows[i].relation.content, rows[i].relation.contentSize);
         }
-        
+        else if (rows[i].type == OUT)
+        {
+            rows[i].outputValue ? printf("true\n") : printf("false\n");
+        }
     } 
 }
 int parseArgs(int argc, char **argv, char **filename)
@@ -314,35 +323,45 @@ void removeEndLine(char *str)
 // parsing the loaded line to extract elements and store them as a Set
 int parseSet(Row *row, char *line)
 {
-
     int contentSize = getContentSize(line);
     char **setContent = (char **)malloc(contentSize * sizeof(char*));
+    enum StateSet state = waitForSpace;
     int elementCounter = 0;
-    int previousPosition = 2;
-    for (int i = 2; line[i] != '\0'; i++)
+
+    for (int i = 0; line[i] != '\0'; i++)
     {
-        if(line[i] == ' ')
+        if(state == waitForSpace)
         {
-            line[i] = '\0';
-            setContent[elementCounter] = &line[previousPosition];
-            previousPosition = i+1; 
+            switch (line[i])
+            {
+                case ' ':
+                    state = readElement;
+                    break;
+                default:
+                    state = waitForSpace;
+                    break;
+            }
+        }
+        else if(state == readElement && isalnum(line[i]))
+        {
+            setContent[elementCounter] = &line[i];
+            state = waitForSpace; 
             elementCounter++;
         }
     }
-    removeEndLine(&line[previousPosition]);
-    setContent[contentSize-1] = &line[previousPosition];
+    
     row->set.content = setContent;
-    row->set.length = ++elementCounter;
+    if(elementCounter == 0)
+        row->set.length = elementCounter;
+    else
+    {
+        row->set.length = elementCounter;
+        replaceSpaceWithZero(line);
+        removeEndLine(setContent[elementCounter-1]);
+    }
     return 1;
 }
-void replaceSpaceWithZero(char *line)
-{
-    for(int i = 0; line[i] != '\0'; i++)
-    {
-        if(line[i] == ' ' || line[i] == ')')
-            line[i] = '\0';
-    }
-}
+
 // parsing relations utilizing a "state machine" and saving them as an array of Pairs
 // looping through the line and checking the parentheses and the spaces
 // calling the replaceSpaceWithZero at the end to ensure that every element of the pair is a signle string without spaces or )
@@ -354,7 +373,7 @@ int parseRelation(Row *row, char *line)
     Pair *pair = (Pair *)malloc(pairCount * sizeof(Pair));
     int iter = 2; // to begin at the first element of the relation
     
-    enum State state = waitForLParentheses; 
+    enum StateRelation state = waitForLParentheses; 
     while(line[iter] != '\n')
     {
         if(state == waitForLParentheses)
@@ -539,16 +558,29 @@ int loadSetsFromFile(Row **rows, FILE *fileptr, int *rowsCount, int *allocatedRo
 
 //commands for sets
 //is the set empty?
-int setIsEmpty(CommandProperties props)
+int setIsEmpty(CommandProperties props, Row **rows)
 {
-    if(->length == 0)
-        return true;
-    return false;
+    int arg = props.arg1-1;
+    int commandPos = props.arg3;
+
+    if((*rows)[arg].type == SET)
+    {
+        (*rows)[commandPos].type = OUT;
+        if((*rows)[arg].set.length == 0)
+        {
+            (*rows)[commandPos].outputValue = true;
+            return true;
+        }
+        (*rows)[commandPos].outputValue = false;
+        return false;
+    }
+    ERROR("There is no set on this line", INVALID_ARGUMENT);
 }
 //number of strings in the set
-int setCard(Set *set){
-    return set->length;
-}
+// int setCard(CommandProperties props)
+// {
+
+// }
 int setContainsString(Set *set, char *str){
     for(int i = 0; i < set->length; i++){
         if(strcmp(set->content[i],str))
@@ -557,7 +589,7 @@ int setContainsString(Set *set, char *str){
     return false;
 }
 //return string array of the complement, dont forget to free() after printing
-char **setComplement(Set *set, Set *uni){
+int setComplement(Set *set, Set *uni){
     char **ret = (char**)malloc(uni->length*sizeof(char*));
     int index = 0;
     for(int i = 0; i < set->length; i++){
@@ -634,14 +666,50 @@ bool setIsSubset(Set *a, Set *b){
 bool setEquals(Set *a, Set *b){
     return a->length == b->length && setIsSubsetOrEq(a, b);
 }
-int executeCommands(Row *rows)
+
+const Command commandList[1] = 
 {
-    for(int i = 0; i < 9; i++)
+    {"empty", setIsEmpty, 1},
+    //{"complement", setComplement, 1},
+    // {"card", setCard, 1},
+    // {"union", setUnion, 2},
+    // {"intersect", setIntersect, 2},
+    // {"minus", setMinus, 2},
+    // {"subseteq", setIsSubsetOrEq, 2},
+    // {"subset", setIsSubset, 2},
+    // {"equals", setEquals, 2}
+};
+int executeCommands(Row **rows, int rowsCount)
+{
+    for(int i = 0; i < rowsCount; i++)
+    {
+        if((*rows)[i].type == COMMAND)
+        {
+            for(int j = 0; j < 1; j++)
+            {
+                if(!strcmp(commandList[j].name, (*rows)[i].command.name))
+                {
+                    int arg1 = (*rows)[i].command.arg1-1;
+                    //int arg2 = rows[i].command.arg2;
+                    if((*rows)[arg1].type == REL || (*rows)[arg1].type == SET)
+                    {
+                        
+                        (*rows)[i].command.arg3 = i;                       
+                        int recVal = commandList[j].func((*rows)[i].command, rows);
+                        if(recVal != 1)
+                            return recVal;
+
+                    }   
+                }
+            }
+        }
+    }
+    return 1;
 }
 int main(int argc, char **argv)
 {
-    char *randomInput[] = {"ahoj", "prd", "test", "nevimnecovelmidlouheho"};
-    printContentNoLength(randomInput);
+    // char *randomInput[] = {"ahoj", "prd", "test", "nevimnecovelmidlouheho"};
+    // printContentNoLength(randomInput);
 
     Row *rows = (Row *)malloc(ROWS_TO_ALLOCATE * sizeof(Row));
     int allocatedRowsCount = ROWS_TO_ALLOCATE;
@@ -657,8 +725,11 @@ int main(int argc, char **argv)
 
     if (loadSetsFromFile(&rows, file, &rowsCount, &allocatedRowsCount) != 1)
         return EMPTY_FILE;
-
-    print(rows, rowsCount);
+    if(executeCommands(&rows, rowsCount) == 1)
+    {
+        print(rows, rowsCount);
+    }
+    
     freeAll(rows, rowsCount);
     fclose(file);
     return 0;
